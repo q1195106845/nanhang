1c37622914fe49d8bb02d81af1e446ded9b4674a
jonesmove <q1195106845@163.com>
2019-02-18 14:46:12 +0800

    Update startVmClient.bat

--- sh/startVmClient.bat
+++ sh/startVmClient.bat
@@ -17,25 +17,29 @@ cd %sc%
 (echo @echo off
 echo set input=%%1
 echo echo %%input%%
+echo set ^"input^=%%input^:^~1^,^-1%%^"
+echo %%input%%
 echo for ^/f ^"delims^=^" %%%%i in ^( ^'reg query ^"HKEY_LOCAL^_MACHINE^\SOFTWARE^\Classes^\vmware^-view^\shell^\open^\command^"^'^) do set yourvar^=%%%%i
 echo echo %%yourvar%%
 echo set ^"acc^=%%yourvar^:^*^"^=%%^"
 echo set ^"sc^=%%acc^:^~0^,^-22%%^"
 echo echo %%sc%%
 echo cd %%sc%%
-echo for /f ^"tokens=2 delims=^$^" %%%%a in ^(^"%%input%%^"^) do set serverUrl=%%%%a
+echo for /f ^"tokens=1 delims=^$^" %%%%a in ^(^"%%input%%^"^) do set serverUrl=%%%%a
 echo echo %%serverUrl%%
-echo for /f ^"tokens=3 delims=^$^" %%%%b in ^(^"%%input%%^"^) do set userName=%%%%b
+echo set ^"serverUrl^=%%serverUrl^:^~9%%^"
+echo for /f ^"tokens=2 delims=^$^" %%%%b in ^(^"%%input%%^"^) do set userName=%%%%b
 echo echo %%userName%%
-echo for /f ^"tokens=4 delims=^$^" %%%%c in ^(^"%%input%%^"^) do set password=%%%%c
+echo for /f ^"tokens=3 delims=^$^" %%%%c in ^(^"%%input%%^"^) do set password=%%%%c
 echo echo %%password%%
-echo for /f ^"tokens=5 delims=^$^" %%%%d in ^(^"%%input%%^"^) do set domainName=%%%%d
+echo for /f ^"tokens=4 delims=^$^" %%%%d in ^(^"%%input%%^"^) do set domainName=%%%%d
 echo echo %%domainName%%
-echo for /f ^"tokens=6 delims=^$^" %%%%e in ^(^"%%input%%^"^) do set desktopName=%%%%e
+echo for /f ^"tokens=5 delims=^$^" %%%%e in ^(^"%%input%%^"^) do set desktopName=%%%%e
 echo echo %%desktopName%%
 echo echo vmware-view ^-^-serverUrl %%serverUrl%% ^-^-userName %%userName%% ^-^-password %%password%% ^-^-domainName %%domainName%% ^-^-desktopName %%desktopName%% ^-nonInteractive
 echo vmware-view ^-^-serverUrl %%serverUrl%% ^-^-userName %%userName%% ^-^-password %%password%% ^-^-domainName %%domainName%% ^-^-desktopName %%desktopName%% ^-nonInteractive)>vm-url-start.bat
-
+set "sc=%sc:\=\\%"
+echo %sc%
 :: 写注册表文件并执行
 (echo Windows Registry Editor Version 5.00
 echo ^[HKEY^_LOCAL^_MACHINE^\SOFTWARE^\Classes^\vmopen^]
@@ -43,19 +47,20 @@ echo ^@^=^"URL^:VMOPEN Protocol^"
 echo ^"URL Protocol^"^=^"^"
 echo ^"UseOriginalUrlEncoding^"^=dword^:00000001
 
-echo ^[HKEY^_LOCAL^_MACHINE^\SOFTWARE^\Classes^\vmopen^\DefaultIcon^]
-echo ^@^=^"C^:^\^\Program Files ^(x86^)^\^\VMware^\^\VMware Horizon View Client^\^\vm-url-start^.bat^,0^"
+echo ^[HKEY^_LOCAL^_MACHINE^\SOFTWARE^\Classes^\vmopen^\DefaultIcon^])>c.reg
 
-echo ^[HKEY^_LOCAL^_MACHINE^\SOFTWARE^\Classes^\vmopen^\shell^]
+echo ^@^=^"%sc%^\^\vm-url-start^.bat^,0^" >>c.reg
 
-echo ^[HKEY^_LOCAL^_MACHINE^\SOFTWARE^\Classes^\vmopen^\shell^\open^]
+echo ^[HKEY^_LOCAL^_MACHINE^\SOFTWARE^\Classes^\vmopen^\shell^] >>c.reg
 
-echo ^[HKEY^_LOCAL^_MACHINE^\SOFTWARE^\Classes^\vmopen^\shell^\open^\command^]
-:: echo
-echo ^@^=^"^\^"C^:^\^\Program Files ^(x86^)^\^\VMware^\^\VMware Horizon View Client^\^\vm-url-start.bat^\^" ^\^"%%1^\^"^")>c.reg
+echo ^[HKEY^_LOCAL^_MACHINE^\SOFTWARE^\Classes^\vmopen^\shell^\open^] >>c.reg
 
+echo ^[HKEY^_LOCAL^_MACHINE^\SOFTWARE^\Classes^\vmopen^\shell^\open^\command^] >>c.reg
+
+echo ^@^=^"^\^"%sc%^\^\vm-url-start.bat^\^" ^\^"%%1^\^"^" >>c.reg
 
 :: 结束
 call c.reg
 
+pause
 pause
